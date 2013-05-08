17bc3da5e5e41e5e613c12c4ef5164aada43cc00
Carlos <Carlos@POR-01>
2013-05-08 19:38:19 +0200

    Modulo Usuarioso

---
+++ module/Campanas/config/module.config.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Campanas\Controller\campanas' => 'Campanas\Controller\campanasController',
+        ),
+    ),
+
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'campanas' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/campanas[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'campanas\Controller\campanas',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'campanas' => __DIR__ . '/../view',
+        ),
+    )
+);
